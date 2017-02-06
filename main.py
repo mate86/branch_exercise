4293780eff49d8a6b55eaa9721e0933a7afa38c9
Balázs Máté <balazsm86@gmail.com>
2017-02-06 19:17:23 +0100

    Created multiple_numbers()

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ def substract_numbers(first, second):
 
 
 substract_numbers(10, 6)
+
+def multiple_numbers(first, second):
+    result = first * second
+    print(result)
+
+multiple_numbers(3, 5)
