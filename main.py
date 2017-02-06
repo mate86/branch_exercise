b6a2b5e9ffa3559b8c562a33968c873eb8dbf7e8
Balázs Máté <balazsm86@gmail.com>
2017-02-06 19:32:18 +0100

    Modified result type in multiple_numbers()

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def substract_numbers(first, second):
 substract_numbers(10, 6)
 
 def multiple_numbers(first, second):
-    result = first * second
+    result = float(first * second)
     print(result)
 
 multiple_numbers(3, 5)
